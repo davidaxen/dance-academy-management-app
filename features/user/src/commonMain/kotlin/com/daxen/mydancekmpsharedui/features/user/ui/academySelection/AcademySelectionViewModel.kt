c53aee97235b2c223bce807f79b2058436a7f5bf
David Bracamonte <david.bracamonte@alumnos.upm.es>
2025-05-03 21:58:14 +0200

    Refactor: Comment out academies property in AcademySelectionViewModel and initialize with an empty list; add academyId to UserAcademyInfo

--- features/user/src/commonMain/kotlin/com/daxen/mydancekmpsharedui/features/user/ui/academySelection/AcademySelectionViewModel.kt
+++ features/user/src/commonMain/kotlin/com/daxen/mydancekmpsharedui/features/user/ui/academySelection/AcademySelectionViewModel.kt
@@ -35,22 +35,23 @@ class AcademySelectionViewModel(
     private val _selectedTab = MutableStateFlow(0)
     val selectedTab: StateFlow<Int> = _selectedTab.asStateFlow()
 
-    private val _academies = MutableStateFlow(listOf(
-        Academy(
-            name = "RLDance",
-            location = "Calle Mayor 123, Madrid",
-            imageUrl = "https://rldancemadrid.my.canva.site/_assets/media/81ffe263615d3538218ffc9f14d339fe.png",
-            schedule = "18:00 - 23:00",
-            role = UserRole.STUDENT,
-        ),
-        Academy(
-            name = "Dance Studio",
-            location = "Avenida Libertad 45, Barcelona",
-            imageUrl = "https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/458941987_1026426806150373_6705277361731783958_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=2Z1vSoUPFL0Q7kNvwEx-b6I&_nc_oc=AdnRE-2GbVyBevZRjS0B3mrws2QTL9SLCGzScmv5ihuu94kqYoLnmY-DpPgzWPghkQ1asC0Ng2pX5k3uEhVlkzf7&_nc_zt=23&_nc_ht=scontent-mad1-1.xx&_nc_gid=HPoB6SrIDxCo3hA9cKY63w&oh=00_AfE1p5XlKAkFowVBEe3i4gwt11LQ2-9lQQ8Gpquci_fybw&oe=6817D5F5",
-            schedule = "09:00 - 22:00",
-            role = UserRole.STUDENT,
-        ),
-    ))
+//    private val _academies = MutableStateFlow(listOf(
+//        Academy(
+//            name = "RLDance",
+//            location = "Calle Mayor 123, Madrid",
+//            imageUrl = "https://rldancemadrid.my.canva.site/_assets/media/81ffe263615d3538218ffc9f14d339fe.png",
+//            schedule = "18:00 - 23:00",
+//            role = UserRole.STUDENT,
+//        ),
+//        Academy(
+//            name = "Dance Studio",
+//            location = "Avenida Libertad 45, Barcelona",
+//            imageUrl = "https://scontent-mad1-1.xx.fbcdn.net/v/t39.30808-6/458941987_1026426806150373_6705277361731783958_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=2Z1vSoUPFL0Q7kNvwEx-b6I&_nc_oc=AdnRE-2GbVyBevZRjS0B3mrws2QTL9SLCGzScmv5ihuu94kqYoLnmY-DpPgzWPghkQ1asC0Ng2pX5k3uEhVlkzf7&_nc_zt=23&_nc_ht=scontent-mad1-1.xx&_nc_gid=HPoB6SrIDxCo3hA9cKY63w&oh=00_AfE1p5XlKAkFowVBEe3i4gwt11LQ2-9lQQ8Gpquci_fybw&oe=6817D5F5",
+//            schedule = "09:00 - 22:00",
+//            role = UserRole.STUDENT,
+//        ),
+//    ))
+    private val _academies = MutableStateFlow<List<Academy>>(emptyList())
     val academies: StateFlow<List<Academy>> = _academies.asStateFlow()
 
     private val _invitations = MutableStateFlow(listOf(
